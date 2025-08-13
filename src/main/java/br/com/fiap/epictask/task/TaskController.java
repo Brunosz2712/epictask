0987cba8e77c48949cf7fe77548d4c793e910021
Brunosz2712 <brunosouza_27@yahoo.com.br>
2025-08-13 17:13:38 -0300

    Colocando inputs, validações e estilização

--- src/main/java/br/com/fiap/epictask/task/TaskController.java
+++ src/main/java/br/com/fiap/epictask/task/TaskController.java
@@ -4,7 +4,9 @@ package br.com.fiap.epictask.task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/task")
@@ -22,4 +24,18 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "index";
     }
+
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String create(Task task, RedirectAttributes redirect) { //sesseion
+        taskService.save(task);
+        redirect.addFlashAttribute("message", "Tarefa Cadastrada com Sucesso!");
+        return "redirect:/task"; //301
+    }
+
+
 }
