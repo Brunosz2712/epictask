a52619b26bb5c641aefe4f942bb99805ad38e768
Brunosz2712 <brunosouza_27@yahoo.com.br>
2025-09-03 16:29:22 -0300

    Atualizando projeto

--- src/main/java/br/com/fiap/epictask/config/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/epictask/config/SecurityConfiguration.java
@@ -12,10 +12,10 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth
-                                .anyRequest().authenticated()
-                        )
-                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests( auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(login -> login.defaultSuccessUrl("/task"))
                 .build();
     }
 
